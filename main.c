03843223912f99af0c5db48fa64ce5f9b34295a5
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-10-06 15:33:39 +0900

    practice 1-2

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[]) {
 	scanf("%i", &number);
 	
 	if(number>0)
-	 printf("양수입니다.");
+	 printf("양수입니다.\n");
 	else if(number<0) 
-	 printf("음수입니다.");
-	else if(number==0)
-	 printf("0 입니다.");
+	 printf("음수입니다.\n");
+	else
+	 printf("0 입니다.\n");
 	  
 	return 0;
 }
